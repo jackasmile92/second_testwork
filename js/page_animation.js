2d6b631a291b10bf71a4d4446ad9930bb94332e9
jackasmile92 <jackasmile92@gmail.com>
2020-05-28 10:39:30 +0300

    banner02 animation

--- js/page_animation.js
+++ js/page_animation.js
@@ -82,6 +82,11 @@ var tl5= gsap.timeline();
 tl5.addLabel("fifth_slide");
 tl5.to("#black_lg",{opacity:1},"fifth_slide");
 tl5.to("#white_lg",{opacity:0},"fifth_slide");
+tl5.from(".banner02__title",{duration:.5,y:100},"fifth_slide");
+tl5.from(".trig_one",{duration:.5,opacity:0},"fifth_slide+=0.5");
+tl5.from(".trig_two",{duration:.5,opacity:0},"fifth_slide+=1 ");
+tl5.from(".trig_three",{duration:.5,opacity:0},"fifth_slide+=1.5");
+tl5.from(".banner02__point",{duration:.5, opacity:0, x:200, stagger:0.5},"fifth_slide+=2");
 
 var scene5  = new ScrollMagic.Scene({reverse:true});
 scene5.setTween(tl5);
