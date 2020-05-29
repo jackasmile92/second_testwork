c8fa79ab31b88097ecb990824f851188390a34c7
jackasmile92 <jackasmile92@gmail.com>
2020-05-29 22:45:45 +0300

    feedback form part two

--- js/feedback_submit.js
+++ js/feedback_submit.js
@@ -1,11 +1,26 @@
-$(document).ready(function(){
-    $("#feedback__top__input").submit(function(e){
-      $("#feedback__top__input").hide();
+$(document).ready(function () {
+    $("#feedback__top__input").submit(function (e) {
+        $("#feedback__top__input").hide();
+        $("#feedback__bottom__input").hide();
 
-        var name =  $('#feedback__top__input').find('input[name="name"]').val();
-
-      $("#feedback__top__info").html(name);
-      e.preventDefault();
+        var name = $('#feedback__top__input').find('input[name="name"]').val();
+        var tel = $('#feedback__top__input').find('input[name="tel"]').val();
+        var answer = name + ', спасибо за ваше сообщение! Мы свяжемся с вами по указанному номеру телефона: ' + tel;
+        $("#feedback__top__info").html(answer);
+        $("#feedback__bottom__info").html(answer);
+        e.preventDefault();
     });
-    
-  });
+
+    $("#feedback__bottom__input").submit(function (e) {
+        $("#feedback__top__input").hide();
+        $("#feedback__bottom__input").hide();
+
+        var name = $('#feedback__bottom__input').find('input[name="name"]').val();
+        var tel = $('#feedback__bottom__input').find('input[name="tel"]').val();
+        var answer = name + ', спасибо за ваше сообщение! Мы свяжемся с вами по указанному номеру телефона: ' + tel;
+        $("#feedback__top__info").html(answer);
+        $("#feedback__bottom__info").html(answer);
+        e.preventDefault();
+    });
+
+});
