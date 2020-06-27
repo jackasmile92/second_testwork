e37c1d292d28ec55da9a237ebaa595b624ff86d8
jackasmile92 <jackasmile92@gmail.com>
2020-06-27 20:42:56 +0300

    fixing several easy fixing moments

--- js/feedback_submit.js
+++ js/feedback_submit.js
@@ -1,26 +1,22 @@
+function feedbackLogic(e) {
+    $("#feedback__top__input").hide();
+    $("#feedback__bottom__input").hide();
+
+    let name = $('#feedback__top__input').find('input[name="name"]').val();
+    let tel = $('#feedback__top__input').find('input[name="tel"]').val();
+    let answer = name + ', спасибо за ваше сообщение! Мы свяжемся с вами по указанному номеру телефона: ' + tel;
+    $("#feedback__top__info").html(answer);
+    $("#feedback__bottom__info").html(answer);
+    e.preventDefault();
+}
+
 $(document).ready(function () {
     $("#feedback__top__input").submit(function (e) {
-        $("#feedback__top__input").hide();
-        $("#feedback__bottom__input").hide();
-
-        var name = $('#feedback__top__input').find('input[name="name"]').val();
-        var tel = $('#feedback__top__input').find('input[name="tel"]').val();
-        var answer = name + ', спасибо за ваше сообщение! Мы свяжемся с вами по указанному номеру телефона: ' + tel;
-        $("#feedback__top__info").html(answer);
-        $("#feedback__bottom__info").html(answer);
-        e.preventDefault();
+        feedbackLogic(e);
     });
 
     $("#feedback__bottom__input").submit(function (e) {
-        $("#feedback__top__input").hide();
-        $("#feedback__bottom__input").hide();
-
-        var name = $('#feedback__bottom__input').find('input[name="name"]').val();
-        var tel = $('#feedback__bottom__input').find('input[name="tel"]').val();
-        var answer = name + ', спасибо за ваше сообщение! Мы свяжемся с вами по указанному номеру телефона: ' + tel;
-        $("#feedback__top__info").html(answer);
-        $("#feedback__bottom__info").html(answer);
-        e.preventDefault();
+        feedbackLogic(e);
     });
 
 });
